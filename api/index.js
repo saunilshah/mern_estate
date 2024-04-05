73d132f03dc0da836f827630df0b14bb196285ed
Saunil Shah <sauniljs12@gmail.com>
2024-04-05 12:59:40 -0400

    created signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose, { Mongoose } from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -10,8 +12,11 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log (err);
 })
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
     }
 )
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
